b6fa34902a0489dca4be79c62587648ade30ca79
Yogesh Jirel <jirelyogesh@gmail.com>
2024-11-28 22:44:22 +0545

    Trying to connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
-import express from 'express';
-
-//app.get("/products", (req,res) => );
+import express, { response } from "express";
+import dotenv from "dotenv";
+import {connectDB} from "./config/db.js";
+dotenv.config();
 const app = express();
+app.get("/", (req,res) => {
+    res.send("Server Started");
+} );
+console.log(process.env.MONGO_URI);
  app.listen(3000, () => {
+    connectDB();
     console.log("Server started at http://localhost:3000");
  });
+
+ 
